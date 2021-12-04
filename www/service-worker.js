e87609359e84ed746641c3fc1f1c6de3a97a6948
Thierry Vilmart <thierry.vilmart@cgi.com>
2021-12-04 22:38:01 +0100

    add try a cache in the service worker

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,8 +1,50 @@
+// source:
+// https://gist.github.com/mohandere/9f293c9d02d094b4c39956c8f3008694
 
-self.addEventListener('install', function(event) {
-    console.log("service worker install");
+var cacheName = 'my-domain1-cache-';
+var staticCacheName = cacheName + '1';
+
+// Cache files
+self.addEventListener('install', function (event) {
+  event.waitUntil(caches.open(staticCacheName).then(function (cache) {
+    return cache.addAll([
+      '/images/wemap-logo.png',
+    ]);
+  }));
 });
 
-self.addEventListener('activate', function(event) {
-    console.log("service worker activate");
+// Remove old data/cache
+self.addEventListener('activate', function (event) {
+  event.waitUntil(caches.keys().then(function (cacheNames) {
+    return Promise.all(cacheNames.filter(function (cacheName) {
+      return cacheName.startsWith(cacheName) && cacheName != staticCacheName;
+    }).map(function (cacheName) {
+      return caches['delete'](cacheName);
+    }));
+  }));
+});
+
+// Serve files from cache
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request, {
+          mode: 'no-cors'
+        }).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  if (!event.data){
+    return;
+  }
+  if ('skipWaiting' === event.data.action) {
+    self.skipWaiting();
+  }
 });
